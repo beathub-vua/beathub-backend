4126dfc288ce77d9487701d1d629526ccc873def
vanjavk <developer@vanjavk.me>
2021-03-17 21:59:06 +0100

    init

--- src/main/java/xyz/beathub/api/ApiApplication.java
+++ src/main/java/xyz/beathub/api/ApiApplication.java
@@ -2,12 +2,20 @@ package xyz.beathub.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApiApplication.class);
     }
 
 }
